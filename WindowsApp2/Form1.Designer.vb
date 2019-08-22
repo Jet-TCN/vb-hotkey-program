cf03526bf42b415d9d928283d6ab1f5d5475bbc4
Jet <jettytjs@gmail.com>
2019-08-23 00:58:49 +0200

    first commit

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.FlavourLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'FlavourLabel
+        '
+        Me.FlavourLabel.AutoSize = True
+        Me.FlavourLabel.Font = New System.Drawing.Font("Arial", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlavourLabel.Location = New System.Drawing.Point(12, 7)
+        Me.FlavourLabel.Name = "FlavourLabel"
+        Me.FlavourLabel.Size = New System.Drawing.Size(117, 24)
+        Me.FlavourLabel.TabIndex = 1
+        Me.FlavourLabel.Text = "Username: "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "MouseData"
+        '
+        'Timer2
+        '
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Hotkey: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 24)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Hotkey: "
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(16, 162)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(64, 64)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Δ"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(86, 162)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(64, 64)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "π"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(156, 162)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(64, 64)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "λ"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(16, 232)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(64, 64)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "¯\_(ツ)_/¯"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(86, 232)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(64, 64)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "™"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(156, 232)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(64, 64)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "²"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Location = New System.Drawing.Point(156, 302)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(64, 64)
+        Me.Button7.TabIndex = 13
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Location = New System.Drawing.Point(86, 302)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(64, 64)
+        Me.Button8.TabIndex = 12
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Location = New System.Drawing.Point(16, 302)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(64, 64)
+        Me.Button9.TabIndex = 11
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(226, 270)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(310, 96)
+        Me.RichTextBox1.TabIndex = 14
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(585, 437)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.FlavourLabel)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Interface"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents FlavourLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,228 @@
+﻿Option Explicit On
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Dim play0, play1, hasnotplayed1, hasnotplayed0 As Boolean
+
+    Dim num() As Integer = {0, 0}
+    Dim delay, delay2, x As Integer
+    Dim operand As Integer = 5
+    Dim strOutput() As String = {"I like to eat pizza"}
+    Dim strExtra, mute, executeFunc As Boolean
+    Dim nullMsg As String = "NULL!"
+    Dim strInput() As String = {nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg, nullMsg}
+
+    Private Const VER_PLATFORM_WIN32_NT = 2
+    Private Const VER_PLATFORM_WIN32_WINDOWS = 1
+    Private Const VK_SCROLL = &H91
+    Private Const KEYEVENTF_EXTENDEDKEY = &H1
+    Private Const KEYEVENTF_KEYUP = &H2
+
+    Public Declare Function GetAsyncKeyState Lib "user32" _
+          Alias "GetAsyncKeyState" (ByVal vKey As Integer) _
+          As Integer
+
+    Private Declare Function SetKeyboardState Lib "user32" _
+   (lppbKeyState As Byte) As Long
+
+
+    Private Declare Sub keybd_event Lib "user32" (
+        ByVal bVk As Byte,
+        ByVal bScan As Byte,
+        ByVal dwFlags As Integer,
+        ByVal dwExtraInfo As Integer
+    )
+
+    Public Sub Scroll_Off()
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY Or 0, 0)
+        keybd_event(VK_SCROLL, &H45, KEYEVENTF_EXTENDEDKEY Or KEYEVENTF_KEYUP, 0)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+        play0 = True
+        play1 = True
+        hasnotplayed1 = True
+        hasnotplayed0 = True
+
+        delay = 100
+        x = 1
+        delay2 = 1
+
+        'MessageBox.Show(Me.Size.ToString)
+
+        My.Forms.Form1.TopMost = True
+        My.Forms.Form1.Left = 0
+        My.Forms.Form1.Top = 0
+        My.Forms.Form1.Size = New Size(292, 65)
+
+        Timer1.Interval = delay
+        Timer1.Start()
+
+        Timer2.Interval = delay2
+        Timer2.Start()
+
+        FlavourLabel.Text = FlavourLabel.Text + My.User.Name
+        'FlavourLabel.ForeColor = Color.Black
+        'FlavourLabel.BackColor = Color.White
+
+        Scroll_Off()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If (GetAsyncKeyState(Keys.F12) And My.Computer.Keyboard.CtrlKeyDown) Then
+            If (My.Forms.Form1.Opacity) = 1 Then
+                My.Forms.Form1.Opacity = 0
+
+            Else
+                My.Forms.Form1.Opacity = 1
+            End If
+        End If
+
+        If (GetAsyncKeyState(Keys.F11) And My.Computer.Keyboard.CtrlKeyDown) Then
+            My.Forms.Form1.Size = New Size(292, 227)
+        Else
+            My.Forms.Form1.Size = New Size(292, 65)
+        End If
+
+        Label3.Text = Math.Round(My.Computer.Info.TotalPhysicalMemory / 1024 / 1024 / 1024) - Math.Round(My.Computer.Info.AvailablePhysicalMemory / 1000000) / 1000 & "GB   /   " &
+         Math.Round(My.Computer.Info.TotalPhysicalMemory / 1024 / 1024 / 1024) & "GB"
+
+        strExtra = My.Computer.Keyboard.ScrollLock
+        strInput(0) = GetAsyncKeyState(Keys.M)
+        strInput(1) = GetAsyncKeyState(Keys.Escape)
+        strInput(2) = GetAsyncKeyState(Keys.NumPad7)
+        strInput(3) = GetAsyncKeyState(Keys.NumPad8)
+        strInput(4) = GetAsyncKeyState(Keys.NumPad9)
+        strInput(5) = GetAsyncKeyState(Keys.NumPad4)
+        strInput(6) = GetAsyncKeyState(Keys.NumPad5)
+        strInput(7) = GetAsyncKeyState(Keys.NumPad6)
+
+        For i = 48 To 57
+            If (GetAsyncKeyState(i) And strExtra) Then
+                ' MessageBox.Show(i-48)
+                RichTextBox1.Text = RichTextBox1.Text + (i - 48).ToString
+                My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            End If
+        Next i
+
+        If (GetAsyncKeyState(Keys.Add) Or GetAsyncKeyState(Keys.Divide) Or GetAsyncKeyState(Keys.Subtract) Or GetAsyncKeyState(Keys.Multiply) And strExtra) Then
+            If (RichTextBox1.Text IsNot "") Then
+                num(0) = RichTextBox1.Text
+                RichTextBox1.Text = ""
+                My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+                If GetAsyncKeyState(Keys.Add) Then
+                    operand = 0
+                ElseIf GetAsyncKeyState(Keys.Subtract) Then
+                    operand = 1
+                ElseIf GetAsyncKeyState(Keys.Multiply) Then
+                    operand = 2
+                ElseIf GetAsyncKeyState(Keys.Divide) Then
+                    operand = 3
+                End If
+            End If
+
+        End If
+
+            If (GetAsyncKeyState(Keys.Enter) And strExtra And RichTextBox1.Text IsNot "") Then
+            num(1) = RichTextBox1.Text
+            RichTextBox1.Text = ""
+            'MsgBox((num(0) + num(1)).ToString)
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            Scroll_Off()
+            executeFunc = True
+            'MessageBox.Show()
+
+        End If
+
+        If executeFunc = True Then
+            If operand = 0 Then
+                My.Computer.Keyboard.SendKeys((num(0) + num(1)).ToString)
+            ElseIf operand = 1 Then
+                My.Computer.Keyboard.SendKeys((num(0) - num(1)).ToString)
+            ElseIf operand = 2 Then
+                My.Computer.Keyboard.SendKeys((num(0) * num(1)).ToString)
+            ElseIf operand = 3 Then
+                My.Computer.Keyboard.SendKeys((num(0) / num(1)).ToString)
+            End If
+
+            executeFunc = False
+        End If
+
+
+
+        If (strExtra) Then
+            Label2.Text = "HotKey: ON"
+            Label2.ForeColor = Color.Green
+        ElseIf (strExtra = False) Then
+            Label2.Text = "HotKey: OFF"
+            Label2.ForeColor = Color.Red
+        End If
+
+        If Label2.ForeColor = Color.Green And hasnotplayed1 = True Then
+            play1 = True
+        ElseIf Label2.ForeColor = Color.Red And hasnotplayed0 = True Then
+            play0 = True
+        End If
+        If mute = False Then
+            If play1 = True Then
+                My.Computer.Audio.Play(My.Resources.hotkey_on, AudioPlayMode.Background)
+                play1 = False
+                hasnotplayed1 = False
+                hasnotplayed0 = True
+            ElseIf play0 = True Then
+                My.Computer.Audio.Play(My.Resources.hotkey_off, AudioPlayMode.Background)
+                play0 = False
+                hasnotplayed0 = False
+                hasnotplayed1 = True
+            End If
+        End If
+
+
+        'RichTextBox1.AppendText(strExtra & strInput(0) & vbNewLine)
+
+        If (strExtra And strInput(3)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("π")
+        ElseIf (strExtra And strInput(1)) Then
+            Close()
+        ElseIf (strExtra And strInput(0)) Then
+            If mute = False Then
+                mute = True
+            Else
+                mute = False
+            End If
+        ElseIf (strExtra And strInput(2)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("Δ")
+            Me.Button1.PerformClick()
+        ElseIf (strExtra And strInput(4)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("λ")
+        ElseIf (strExtra And strInput(5)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("¯\_(ツ)_/¯")
+        ElseIf (strExtra And strInput(6)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("™")
+        ElseIf (strExtra And strInput(7)) Then
+            My.Computer.Keyboard.SendKeys("{BACKSPACE}")
+            My.Computer.Keyboard.SendKeys("²")
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Label1.Text = "Mouse X: " + MousePosition.X.ToString + "  Mouse Y: " + MousePosition.Y.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+End Class
